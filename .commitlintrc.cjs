8e5377c8354b4fd55a6c92a8b908fd9f27f52dfc
junsyun22 <junsyun22@gmail.com>
2024-08-27 17:27:19 +0900

    Chore: update commitlint configuration for commit message conventions
    - 커밋 메시지 타입 규칙(type-enum) 수정: 'Feat', 'Fix', 'Docs' 등 첫 글자만 대문자로 제한
    - 타입 대소문자 규칙(type-case) 추가: pascal-case로 설정하여 타입의 첫 글자만 대문자로 설정
    - 제목 비어 있음(subject-empty) 규칙 추가: 제목이 비어 있을 수 없도록 설정
    - 제목 대소문자 규칙(subject-case) 수정: 제목의 대소문자 규칙을 무시하도록 설정
    - 제목 최대 길이(header-max-length) 제한 설정: 제목의 최대 길이를 72자로 제한
    - Merge 커밋은 무시하도록 설정
    
    이 변경을 통해 일관된 커밋 메시지 규칙을 강제하고, 코드 품질과 가독성을 유지하기 위함입니다.

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -5,20 +5,22 @@ module.exports = {
       2,
       'always',
       [
-        'Feat', // 새로운 기능 추가
-        'Fix', // 버그 수정
-        'Docs', // 문서 수정
-        'Style', // 코드 포맷팅, 세미콜론 누락 등
-        'Refactor', // 코드 리팩토링
-        'Test', // 테스트 코드 추가
-        'Chore', // 빌드 업무 수정 등
-        'Comment', // 주석 추가 및 변경
-        'Remove', // 파일, 폴더 삭제
-        'Rename', // 파일, 폴더명 수정
+        'Feat',      // 새로운 기능 추가
+        'Fix',       // 버그 수정
+        'Docs',      // 문서 수정
+        'Style',     // 코드 포맷팅, 세미콜론 누락 등
+        'Refactor',  // 코드 리팩토링
+        'Test',      // 테스트 코드 추가
+        'Chore',     // 빌드 업무 수정 등
+        'Comment',   // 주석 추가 및 변경
+        'Remove',    // 파일, 폴더 삭제
+        'Rename',    // 파일, 폴더명 수정
+        'CI'         // 통합 관련 작업
       ],
     ],
+    'type-case': [2, 'always', 'pascal-case'], // 타입은 첫 글자만 대문자
     'subject-empty': [2, 'never'], // 제목은 비어있을 수 없음
-    'subject-case': [2, 'always', ['sentence-case']], // 제목은 문장형식 (첫 글자만 대문자)
+    'subject-case': [2, 'never', []], // 제목의 대소문자 규칙 무시
     'header-max-length': [2, 'always', 72], // 제목 최대 길이 제한
   },
   ignores: [
