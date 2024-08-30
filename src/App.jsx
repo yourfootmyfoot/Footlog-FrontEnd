ec143b332d00011f94657c3a3cccc584e942a04d
junsyun22 <junsyun22@gmail.com>
2024-08-30 20:35:25 +0900

    Feat: BottomSheet 구현중

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import theme from './theme/theme';
 import Landing from './pages/Landing';
 import Login from './pages/Login/Login';
 import Footer from './components/Layout/Footer';
+import Match from './pages/Match/Match';
 
 const App = () => {
   return (
@@ -14,8 +15,8 @@ const App = () => {
         <Switch>
           <Route path="/" exact component={Landing} />
           <Route path="/login" component={Login} />
-          {/*<Route path="/match" component={Match} />
-          <Route path="/club" component={Club} />
+          <Route path="/match" component={Match} />
+          {/*<Route path="/club" component={Club} />
           <Route path="/profile" component={Profile} />
           <Route path="/guest" component={Guest} />
           <Route path="/settings" component={Settings} />*/}
