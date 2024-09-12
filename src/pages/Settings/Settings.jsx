2e6adb560132280251484e9705dafa0d9dd987cf
Cheong Ye Jun <jjyejun@naver.com>
2024-09-12 14:55:10 +0900

    Feat: 설정 페이지 로그아웃, 1대1 문의 추가, CSS 추가

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -11,6 +11,14 @@ const Settings = () => {
     alert('변경 사항이 저장되었습니다!');
   };
 
+  const handleLogout = () => {
+    alert('로그아웃되었습니다!');
+  };
+
+  const handleInquiry = () => {
+    alert('1대1 문의로 이동합니다!');
+  };
+
   return (
     <div className="settings-page">
       {/* 닉네임 변경 */}
@@ -22,6 +30,12 @@ const Settings = () => {
           onChange={(e) => setNickname(e.target.value)} 
           placeholder="새로운 닉네임을 입력하세요."
         />
+      </div>
+
+      {/* 1대1 문의 */}
+      <div className="setting-item">
+        <h3>1대1 문의</h3>
+        <button className="inquiry-button" onClick={handleInquiry}>문의하기</button>
       </div>
 
       {/* 알림 설정 */}
@@ -62,7 +76,9 @@ const Settings = () => {
         </div>
       </div>
 
+      {/* 저장 및 로그아웃 버튼 */}
       <div className="save-button-container">
+        <button className="logout-button" onClick={handleLogout}>로그아웃</button>
         <button className="save-button" onClick={handleSave}>저장</button>
       </div>
     </div>
