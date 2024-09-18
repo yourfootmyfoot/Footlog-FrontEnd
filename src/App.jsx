b9e4de1932c98aae4219cef49392bf1cd1f16033
junsyun22 <junsyun22@gmail.com>
2024-09-18 15:51:31 +0900

    refactor: 내용 정리
    - import문 그룹화
    - 라우트

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,28 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
 import GlobalStyles from './theme/GlobalStyles';
 import theme from './theme/theme';
-import Landing from './pages/Landing';
-import Login from './pages/Login/Login';
-import MatchEnrollForm from './pages/MatchEnroll/MatchEnrollForm';
+
+// Layout components
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import AppContainer from './components/layout/AppContainer/AppContainer';
+
+// Page components
+import Landing from './pages/Landing';
+import Login from './pages/Login/Login';
 import Match from './pages/Match/Match';
 import MatchDetail from './pages/Match/MatchDetail';
-import AppContainer from './components/layout/AppContainer/AppContainer';
+import MatchEnrollForm from './pages/MatchEnroll/MatchEnrollForm';
+import ChatRoomListPage from './pages/Chat/ChatRoomListPage';
+import ClubList from './pages/ClubList/pages/ClubList';
+import Settings from './pages/Settings/Settings';
+
+// Mercenary related components
 import Mercenary from './pages/Mercenary/pages/MercenaryChoice';
 import MercenaryAppList from './pages/Mercenary/pages/MercenaryApp/MercenaryAppList';
 import MercenaryRecList from './pages/Mercenary/pages/MercenaryRec/MercenaryRecList';
-import EnrollMercenaryRec from './pages/Mercenary/pages/EnrollMercenaryRec/EnrollMercenaryRec';
-import ClubList from './pages/ClubList/pages/ClubList';
-import Settings from './pages/Settings/Settings';
 import EnrollMercenaryApp from './pages/Mercenary/pages/EnrollMercenaryApp/EnrollMercenaryApp';
-
+import EnrollMercenaryRec from './pages/Mercenary/pages/EnrollMercenaryRec/EnrollMercenaryRec';
 
 const App = () => {
   return (
@@ -27,23 +33,31 @@ const App = () => {
         <AppContainer>
           <Header />
           <Switch>
-            <Route path="/" exact component={Landing} />
+            <Route exact path="/" component={Landing} />
             <Route path="/login" component={Login} />
-            <Route path="/match" component={Match} />
-            <Route path="/matchDetail" component={MatchDetail} />
-            <Route path="/Mercenary" component={Mercenary} />
-            <Route path="/MercenaryAppList" component={MercenaryAppList} />
-            <Route path="/MercenaryRecList" component={MercenaryRecList} />
-            <Route path="/EnrollMercenaryApp" component={EnrollMercenaryApp} />
-            <Route path="/ClubList" component={ClubList} />
-            <Route path="/match-enroll" component={MatchEnrollForm} />
-            <Route path="/EnrollMercenaryRec" component={EnrollMercenaryRec} />
-            {/*<Route path="/club" component={Club} />
-            <Route path="/profile" component={Profile} />*/}
+            
+            {/* Match related routes */}
+            <Route exact path="/match" component={Match} />
+            <Route path="/match/detail" component={MatchDetail} />
+            <Route path="/match/enroll" component={MatchEnrollForm} />
+            
+            {/* Mercenary related routes */}
+            <Route exact path="/mercenary" component={Mercenary} />
+            <Route path="/mercenary/app-list" component={MercenaryAppList} />
+            <Route path="/mercenary/rec-list" component={MercenaryRecList} />
+            <Route path="/mercenary/enroll-app" component={EnrollMercenaryApp} />
+            <Route path="/mercenary/enroll-rec" component={EnrollMercenaryRec} />
+            
+            <Route path="/club-list" component={ClubList} />
+            <Route path="/chat" component={ChatRoomListPage} />
             <Route path="/settings" component={Settings} />
+            
+            {/* Uncomment these routes when components are ready */}
+            {/*<Route path="/club" component={Club} />*/}
+            {/*<Route path="/profile" component={Profile} />*/}
           </Switch>
+          <Footer />
         </AppContainer>
-        <Footer />
       </Router>
     </ThemeProvider>
   );
